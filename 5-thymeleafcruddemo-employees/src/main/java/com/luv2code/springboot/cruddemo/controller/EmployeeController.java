3aacc191040d21e7f323a06bbd1bb9185a8c083b
Thao Nguyen <thaontt1408@gmail.com>
2022-06-13 22:08:43 -0600

    Add link to /list

--- 5-thymeleafcruddemo-employees/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ 5-thymeleafcruddemo-employees/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -1,9 +1,14 @@
 package com.luv2code.springboot.cruddemo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 
 @Controller
@@ -16,4 +21,16 @@ public class EmployeeController {
 	public EmployeeController(EmployeeService theEmployeeService) {
 		employeeService = theEmployeeService;
 	}
+	
+	@GetMapping("/list")
+	public List<Employee> findAllEmployees(){
+		return employeeService.findAllEmployees();
+	}
+	
+	@GetMapping("list/{theId}")
+	public Employee findEmployeeById(@PathVariable int theId) {
+		return employeeService.findEmployeeById(theId);
+	}
+	
+	
 }
