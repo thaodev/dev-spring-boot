b10e772967cf1264ddd914e8f3a38ccdfc825391
Thao Nguyen <thaontt1408@gmail.com>
2022-06-14 05:58:44 -0600

    Add methods for different activities

--- 5-thymeleafcruddemo-employees/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ 5-thymeleafcruddemo-employees/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.entity.Salary;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 
 @Controller
@@ -23,14 +26,48 @@ public class EmployeeController {
 	}
 	
 	@GetMapping("/list")
-	public List<Employee> findAllEmployees(){
-		return employeeService.findAllEmployees();
+	public String findAllEmployees(Model theModel){
+		List<Employee> theEmployees = employeeService.findAllEmployees();
+		
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "employees/list-employees";
 	}
 	
-	@GetMapping("list/{theId}")
-	public Employee findEmployeeById(@PathVariable int theId) {
-		return employeeService.findEmployeeById(theId);
+	//this is only for API
+//	@GetMapping("list/{theId}")
+//	public Employee findEmployeeById(@PathVariable int theId) {
+//		return employeeService.findEmployeeById(theId);
+//	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		//create model attribute to bind form data
+		Employee theEmployee = new Employee();
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
 	}
 	
+	@GetMapping("/showSalaryDepartment")
+	public String showSalaryDepartment(Model theModel) {
+		
+		//create model attribute to bind form data
+		Salary theSalary = new Salary();
+		
+		theModel.addAttribute("salary", theSalary);
+		
+		return "employees/salary-structure-form";
+	}
+	
+	@GetMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		//save Employee
+		employeeService.saveEmployee(theEmployee);
+		
+		return "redirect:/employees/list";
+	}
 	
 }
