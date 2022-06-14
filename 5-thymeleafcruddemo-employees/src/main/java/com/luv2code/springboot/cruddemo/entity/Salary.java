1a6d64fb5d838c9115875270e01bdaca5debdb68
Thao Nguyen <thaontt1408@gmail.com>
2022-06-14 06:00:52 -0600

    Add Salary entity

---
+++ 5-thymeleafcruddemo-employees/src/main/java/com/luv2code/springboot/cruddemo/entity/Salary.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.cruddemo.entity;
+
+public class Salary {
+	
+	private String jobCode;
+	private String department;
+	private int amount;
+	
+	
+	public String getJobCode() {
+		return jobCode;
+	}
+	public void setJobCode(String jobCode) {
+		this.jobCode = jobCode;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	
+
+}
