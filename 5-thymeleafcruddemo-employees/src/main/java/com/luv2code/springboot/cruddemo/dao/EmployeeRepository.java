892a547b8e49bdb46481842beb8e05e827c6bbb6
Thao Nguyen <thaontt1408@gmail.com>
2022-06-13 22:09:33 -0600

    Add the findAllEmployeebysoring

--- 5-thymeleafcruddemo-employees/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 5-thymeleafcruddemo-employees/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.luv2code.springboot.cruddemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
+	public List<Employee> findAllByOrderByLastNameAsc();
 }
